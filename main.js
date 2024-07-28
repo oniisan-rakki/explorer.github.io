23a847013063b19e22fcc1e267d5d4b45fee37b3
oniisan-rakki <153196954+oniisan-rakki@users.noreply.github.com>
2024-07-28 23:59:32 +0200

    responsive + javascript

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 // showOffers()
 // showExperiences()
 // openChat()
-// openMobileMenu()
+
+// Mobile Menu
+let openMenu = document.getElementById("hamburger");
+let closeMenu = document.getElementById("close");
+let mobileMenu = document.getElementById("mobileMenu");
+openMenu.addEventListener("click", function() {
+    openMenu.style.display = "none";
+    closeMenu.style.display = "grid";
+    mobileMenu.style.display = "grid";
+});
+closeMenu.addEventListener("click", function() {
+    openMenu.style.display = "grid";
+    closeMenu.style.display = "none";
+    mobileMenu.style.display = "none";
+});
+
 // goToNextSectionHome()
 // goToNextSectionAccommodation()
 // goToNextSectionGroupBookings()
